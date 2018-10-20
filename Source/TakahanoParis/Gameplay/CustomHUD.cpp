52b8ad9aa6c995c0be32846bc23c75187ffae09c
PERARD-G N <perard.git@outlook.com>
2018-10-20 01:11:37 -0400

    Fixed CustomHUD, Added AIModule to project
    HUD was exponentially adding actors to the Array of actors in the middle of screen. Also removed Log for it.
    Adding AI module to ".uproject" file

--- Source/TakahanoParis/Gameplay/CustomHUD.cpp
+++ Source/TakahanoParis/Gameplay/CustomHUD.cpp
@@ -23,8 +23,8 @@ void ACustomHUD::SetClassFilter(TSubclassOf<AActor> NewClassFilter)
 
 bool ACustomHUD::GetActorsInCenterofScreen_Backend(TArray<AActor*>& OutActors)
 {
-	FVector2D CenterOfScreen(Canvas->SizeX/2.f, Canvas->SizeY/2.f);
+	OutActors.Empty();
+	const FVector2D CenterOfScreen(Canvas->SizeX/2.f, Canvas->SizeY/2.f);
 	GetActorsInSelectionRectangle<AActor>(CenterOfScreen - CenterSpan, CenterOfScreen + CenterSpan, OutActors, true, false); // Using default values for the rest
-	UE_LOG(LogTemp, Warning, TEXT("There are : %d actors under middle of cursor"), OutActors.Num());
 	return OutActors.Num() > 0;
 }
