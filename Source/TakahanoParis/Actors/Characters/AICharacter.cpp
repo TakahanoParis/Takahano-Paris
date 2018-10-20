a93cd23a61407b83e2fb91e6659ab654a9a41373
PERARD-G N <perard.git@outlook.com>
2018-10-20 01:08:40 -0400

    Updated AICharacter

--- Source/TakahanoParis/Actors/Characters/AICharacter.cpp
+++ Source/TakahanoParis/Actors/Characters/AICharacter.cpp
@@ -1,7 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "AICharacter.h"
+#include "UnrealNetwork.h"
 
 
 
+void AAICharacter::GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AAICharacter, PatrolPath);
 
+
+}
+
+void AAICharacter::BeginPlay()
+{
+	const auto AI = GetAIController();
+	if(AI && PatrolPath)
+	{
+		AI->SetPathActor(PatrolPath);
+		AI->StartPatrol();
+	}
+}
--- Source/TakahanoParis/Actors/Characters/AICharacter.h
+++ Source/TakahanoParis/Actors/Characters/AICharacter.h
@@ -4,17 +4,37 @@
 
 #include "CoreMinimal.h"
 #include "Actors/Characters/BaseCharacter.h"
+#include "Gameplay/CustomAIController.h"
 #include "AICharacter.generated.h"
 
 /**
- * 
+ *	@class AAICharacter
+ *	@brief Base class for AI character
  */
 UCLASS()
 class TAKAHANOPARIS_API AAICharacter : public ABaseCharacter
 {
 	GENERATED_BODY()
-	
-	
-	
+
+public:
+
+	// Called at Spawn or level start
+	void BeginPlay() override;
+
+	/**
+	 *	@property PatrolPath
+	 *	@brief Path to follow
+	 */
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "IA")
+	class ASplinePathActor * PatrolPath;
+
+	/**
+	 *	@fn	GetAIController()
+	 *	@return The CustomAIController for that actor
+	 */
+	UFUNCTION()
+		ACustomAIController * GetAIController() const {return Cast<ACustomAIController>(GetController());}
+
+
 	
 };
