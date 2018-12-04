ffe8479c3a4dc5f700e0620b40bab686b4a0bef0
PERARD-G N <perard.git@outlook.com>
2018-12-04 17:33:50 -0500

    added video widget class
    Video widget should be able to play videos in a media player

---
+++ Source/TakahanoParis/UserInterface/VideoWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UserInterface/CustomWidget.h"
+#include "VideoWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TAKAHANOPARIS_API UVideoWidget : public UCustomWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
